6f327b0417eaec3724e2304bd3ff4c345a6bf3eb
mouzam <mzmjuly9@gmail.com>
2020-10-18 21:28:37 +0530

    connected moongose to mogodb

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 const ejs = require("ejs");
 const multer = require('multer');
 const cors =require('cors');
+const mongoose = require('mongoose');
 
 const app=express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology:true});
 
 
 app.get("/",function(req,res){
