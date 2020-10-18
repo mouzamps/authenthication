042bf4cbdc605a3ae329ee1629e0ce31cb971284
mouzam <mzmjuly9@gmail.com>
2020-10-18 21:51:31 +0530

    added schema,model,and save to db the users

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology:true});
 
+const userSchema = {
+    email: String,
+    password: String
+};
+const User = new mongoose.model("User",userSchema);
+
 
 app.get("/",function(req,res){
     res.render("home")
@@ -23,7 +29,19 @@ app.get("/login",function(req,res){
 app.get("/register",function(req,res){
     res.render("register")
 });
-
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }else {
+            res.render("secrets");
+        }
+    });
+});
 
 
 
