530f9ffedcc0c01011b31aa8bf445951123f1e3d
mouzam <mzmjuly9@gmail.com>
2020-10-19 23:00:24 +0530

    added mongoose-encrytion code for password

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const ejs = require("ejs");
 const multer = require('multer');
 const cors =require('cors');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app=express();
 
@@ -13,10 +14,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology:true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+const secret = "thisismysecret";
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
 const User = new mongoose.model("User",userSchema);
 
 
