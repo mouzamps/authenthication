c33f861a04a8893c9783f32a404583d0afc211e2
mouzam <mzmjuly9@gmail.com>
2020-10-20 23:09:28 +0530

    added .env file

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "thisismysecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema);
 
