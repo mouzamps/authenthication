861605ed4f4c202093eef08ddc030c8fa644c48d
mouzam <mzmjuly9@gmail.com>
2020-10-21 23:21:45 +0530

    removed encrytion and installed md5 hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const ejs = require("ejs");
 const multer = require('multer');
 const cors =require('cors');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app=express();
 
@@ -21,7 +22,6 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User",userSchema);
 
@@ -38,7 +38,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err){
         if(err){
